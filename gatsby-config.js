362351873b02808efb563e7dac24d8c0abbb1ad3
omlinson <philipatomlinson@gmail.com>
2023-12-19 15:46:25 -0500

    testing why deploy broke

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
+//require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
 
 module.exports = {
   siteMetadata: {
