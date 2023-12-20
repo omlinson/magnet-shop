c30ba1089bfed88f61ac996cee3231d6df7b5bdf
omlinson <philipatomlinson@gmail.com>
2023-12-20 12:10:39 -0500

    need to do better QA Lol

--- src/templates/tagTemplate.js
+++ src/templates/tagTemplate.js
@@ -35,14 +35,31 @@ const TagTemplate = ({ data, pageContext }) => {
           const imageData = imageMap.get(magnet.image);
           const slug = magnet.name ? slugify(magnet.name) : '#';
           return (
-            <Link to={magnet.name ? `/fridge-magnets/${slug}` : '#'} key={index}>
-              {imageData && (
-                <GatsbyImage
-                  image={getImage(imageData.gatsbyImageData)}
-                  alt={`Fridge Magnet ${magnet.name}`}
-                />
-              )}
-            </Link>
+            <div key={index} style={{ position: 'relative' }}>
+                <Link to={magnet.name ? `/fridge-magnets/${slug}` : '#'} key={index}>
+                {imageData && (
+                    <GatsbyImage
+                    image={getImage(imageData.gatsbyImageData)}
+                    alt={`Fridge Magnet ${magnet.name}`}
+                    />
+                )}
+                </Link>
+                {magnet.availability !== "in stock" && (
+                    <div style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        backgroundColor: 'rgba(0, 0, 0, 1)',
+                        color: 'white',
+                        padding: '10px',
+                        textAlign: 'center',
+                        textTransform: 'uppercase'
+                        }}>
+                        {magnet.availability}
+                    </div>
+                    )}
+            </div>
           );
         })}
       </div>
@@ -50,12 +67,15 @@ const TagTemplate = ({ data, pageContext }) => {
   )
 }
 
+
+
 export const query = graphql`
   query($tag: String!) {
     allGooglePimSheet(filter: {tag1: {eq: $tag}}) {
       nodes {
         name
         image
+        availability
       }
     }
     allFile {
