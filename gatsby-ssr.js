881f6829685ef1871484b429881bebc2aaa00f28
omlinson <philipatomlinson@gmail.com>
2024-01-09 11:12:03 -0500

    did I fix it

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,23 +4,16 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
+const React = require('react');
+const { WishlistProvider } = require('./src/context/wishlistContext');
+
+exports.wrapRootElement = ({ element }) => {
+  return <WishlistProvider>{element}</WishlistProvider>;
+};
+
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
 exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
 }
-
-
-import React from 'react'; 
-import { WishlistProvider } from './src/context/wishlistContext.js'; 
-
-const RootComponent = ({element}) => {
-  return (
-    <WishlistProvider>
-      {element}
-    </WishlistProvider>
-  );
-};
-
-export const wrapRootElement = RootComponent;
