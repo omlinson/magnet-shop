6075eef87dc10bc018c73c19a8095ecc8e3ec203
omlinson <philipatomlinson@gmail.com>
2024-01-11 11:41:39 -0500

    switched manifest a bit

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `omlinson`,
+        short_name: `omlinson`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
