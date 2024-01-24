5966cc86165ca1e1d541b973d7bf437b71421b58
omlinson <philipatomlinson@gmail.com>
2024-01-24 12:20:51 -0500

    fixed stupid issue

--- src/components/header.js
+++ src/components/header.js
@@ -10,25 +10,9 @@ const Header = ({ siteTitle }) => {
 
   const { consentDefined, handleConsentChange } = useWishlist();
 
-    /* keeping in case issue happens again if (consentDefined === null) {
-      return (<header className={`${consentDefined ? 'defined' : 'undefined'}`}>
-      
-    <div className="headerTopRow">
-      <div style={{ display: 'flex', alignItems: 'baseline', gap:`10px`  }}>
-        <BurgerMenu />
-        <Logo siteTitle={siteTitle} />
-      </div>
-      <div style={{ display: 'flex', alignItems: 'baseline', gap:`15px` }}>
-        <NavWishlist />
-      </div>
-    </div>
-    <NavBar />
- </header>)
-    } */
-
     return (
       <header className={`${consentDefined ? 'defined' : 'undefined'}`}>
-          {consentDefined !== null && !consentDefined && (
+          {consentDefined === null && !consentDefined && (
         <div className="consent undefined">
           <Consent onConsentChange={handleConsentChange} />
         </div>
@@ -39,10 +23,9 @@ const Header = ({ siteTitle }) => {
             <Logo siteTitle={siteTitle} />
           </div>
           <div style={{ display: 'flex', alignItems: 'baseline', gap:`15px` }}>
-            
+          <NavWishlist />
             {consentDefined && (  
               <>
-                <NavWishlist />
                 <div  className="defined">
                 <Consent onConsentChange={handleConsentChange} consent={consentDefined} /> 
               </div>
